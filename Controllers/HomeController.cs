187ac094440dffac0da325272cee69c5e7d20675
bkh-son <buikhachason@gmail.com>
2021-03-21 22:34:32 +0700

    Update API

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyWebApp.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MyWebApp.Controllers
@@ -26,6 +30,60 @@ namespace MyWebApp.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [Route("read")]
+        public async Task<IActionResult> ReadMessagePage()
+        {
+            List<Message> lsMessage = new List<Message>();
+            HttpClient httpClient = new HttpClient();
+            var apiResponse = await httpClient.GetAsync("https://localhost:5001/mes");
+            try
+            {
+                string response = await apiResponse.Content.ReadAsStringAsync();
+                response = "{\"data\":" + response + "}";
+                JObject jObResponse = JObject.Parse(response);
+                lsMessage = JsonConvert.DeserializeObject<List<Message>>(jObResponse["data"].ToString());
+            }
+            catch(Exception ex)
+            {
+                lsMessage = null;
+            }
+            return View(lsMessage);
+        }
+        [Route("readnew")]
+        public async Task<IActionResult> ReadNewMessagePage()
+        {
+            List<Message> lsMessage = new List<Message>();
+            HttpClient httpClient = new HttpClient();
+            var apiResponse = await httpClient.GetAsync("https://localhost:5001/mes/new");
+            try
+            {
+                string response = await apiResponse.Content.ReadAsStringAsync();
+                response = "{\"data\":" + response + "}";
+                JObject jObResponse = JObject.Parse(response);
+                lsMessage = JsonConvert.DeserializeObject<List<Message>>(jObResponse["data"].ToString());
+            }
+            catch (Exception ex)
+            {
+                lsMessage = null;
+            }
+            return View(lsMessage);
+        }
+
+        public async Task<IActionResult> CheckRead(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            var httpContent = new StringContent(null, Encoding.UTF8, "application/json"); ;
+            var apiResponse = await httpClient.PutAsync("https://localhost:5001/mes/" + id.ToString(), httpContent);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Delte(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            var apiResponse = await httpClient.DeleteAsync("https://localhost:5001/mes/" + id.ToString());
+            return RedirectToAction("Index", "Home");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApp.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BodyMessage { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
